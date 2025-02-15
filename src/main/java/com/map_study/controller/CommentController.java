86abd42577d2a7084ad9dd6cb44ecff139f215f8
EunBi Ko <e_un_04@naver.com>
2025-02-15 22:53:42 +0900

    RestController 수정

--- src/main/java/com/map_study/controller/CommentController.java
+++ src/main/java/com/map_study/controller/CommentController.java
@@ -2,13 +2,11 @@ package com.map_study.controller;
 
 import com.map_study.entity.Comment;
 import com.map_study.service.CommentService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/comment")
 public class CommentController {
 
